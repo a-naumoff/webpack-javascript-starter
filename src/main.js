66fb9ed8ef0959425cea66223f592de9ad82a7aa
Andrey Naumov <andreu.naumoff@gmail.com>
2020-12-18 22:39:37 +0300

    remove README.md

--- src/main.js
+++ src/main.js
@@ -1,9 +1,1 @@
 import './styles.scss';
-import { render } from './app/test';
-
-const $app = document.getElementById('app');
-
-const $el = document.createElement('div');
-$el.insertAdjacentHTML('beforeend', render());
-
-$app.append($el);
